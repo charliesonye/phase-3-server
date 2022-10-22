142c9325c816a9e8b0c4c8e60815931dc90000a2
Charles <c_onyemem@live.com>
2022-10-22 14:21:29 -0700

    Started defining how data will be sent back to frontend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,28 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/" do 
+    "Let this server as the Home page message".to_json
+  end
+
+  get "/technicians" do
+    techies = Technician.all
+    techies.to_json
+  end
+
+  get "/technicians/:id" do
+    techie = Technician.find_by(id: params[:id])
+    techie.to_json(include: :customers)
+  end
+
+  get "/customer/:id" do 
+    customer = Customer.find_by(id: params[:id])
+    if customer.item_returned
+    customer.to_json
+    else
+      customer.item_returned = "Pending"
+      customer.to_json
+    end
   end
 
 end
